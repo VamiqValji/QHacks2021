564dc46854dddfa1e96589226c82f6a71d41c864
Vamiq <vamiq.valji@gmail.com>
2021-01-23 03:22:18 -0500

    Fixed React Router Routing / Fixed Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Restaurants from "./pages/Restaurants";
 import SignIn from "./pages/SignIn";
+import SignUp from './pages/SignUp';
 
 import {
   BrowserRouter as Router,
@@ -15,6 +16,7 @@ export default function App() {
     <header className="App-header">
     <Router>
       <div>
+        <div className="nav">
         <nav>
           <ul>
             <li>
@@ -29,20 +31,26 @@ export default function App() {
             <li>
               <Link to="/signin">Sign In</Link>
             </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
           </ul>
         </nav>
+        </div>
         <Switch>
           <Route path="/restaurants">
             <Restaurants />
           </Route>
           <Route path="/signin">
-            
+            <SignIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
           </Route>
         </Switch>
       </div>
     </Router>
     </header>
-    <Restaurants/>
     </div>
   );
 }
