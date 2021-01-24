3de1ef0b277f508092fe89771312eb029d257744
Vamiq <vamiq.valji@gmail.com>
2021-01-24 05:30:04 -0500

    login / sign up feedback added

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,17 @@
-import React, {useContext} from "react";
+import React, { useState } from "react";
 import "../App.css";
- import "./Signin.css";
+import "./Signin.css";
 import axios from "axios";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+// import { Link } from "react-router-dom";
 
 export default function SignIn() {
+  const [feedback, setFeedback] = useState();
+
+  const success = <div className="success">Sign In Successful!</div>;
+  const fail = <div className="fail">Failed Login.</div>;
+
   const submit = (e) => {
     e.preventDefault();
     let EMAIL = document.getElementById("EMAIL").value;
@@ -24,56 +30,86 @@ export default function SignIn() {
         const res = await fetch("http://localhost:3001/signin/logged");
         data = await res.json();
         console.log(data);
-      } catch(err) {
-        console.log(err)
+      } catch (err) {
+        console.log(err);
       }
-      console.log(data)
-      console.log(EMAIL)
-      data.forEach(profile => {
+      console.log(data);
+      console.log(EMAIL);
+      setFeedback(fail);
+      data.forEach((profile) => {
         if (profile.email === EMAIL) {
-          console.log("logged in")
-          console.log(profile)
-          console.log(profile.email)
-          console.log(profile.organization)
-          sessionStorage.setItem("user",[profile.email, profile.organization, profile._id])
+          setFeedback(success);
+          console.log("logged in");
+          console.log(profile);
+          console.log(profile.email);
+          console.log(profile.organization);
+          sessionStorage.setItem("user", [
+            profile.email,
+            profile.organization,
+            profile._id,
+          ]);
+        } else {
+          console.log("Invalid.");
         }
       });
+    };
+    // useEffect(() => {
+    //   getData();
+    // }, []);
+    getData();
   };
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-  getData();
-
-  }
 
   return (
     <div className="signinn">
       <header className="App-header">
         <h1 className="signInPage">Sign In</h1>
 
-         <div className = "div">
-        <form onSubmit={submit} className="form">
+        <div className="div">
+          <form onSubmit={submit} className="form">
+            <div className="input">
+              <label className="labels" for="Email">
+                Email:
+              </label>
 
-        <div className="input">
-        
-          <label className= "labels" for="Email">Email:</label>
-
-          <br />
-          <TextField  className = "email" id="EMAIL" label="Email" name = "email" required/>
-       
+              <br />
+              <TextField
+                className="email"
+                id="EMAIL"
+                label="Email"
+                name="email"
+                required
+              />
+            </div>
+            <div className="Password">
+              <label className="labels" for="password">
+                Password:
+              </label>
+              <br />
+              <TextField
+                className="password"
+                id="PASSWORD"
+                label="Password"
+                name="password"
+                required
+              />
+            </div>
+            <div className="elly">
+              {/* <Link to="/signincheck" style={{ textDecoration: 'none' }} > */}
+              <Button
+                type="submit"
+                className="fullButton"
+                variant="contained"
+                color="primary"
+                required
+              >
+                Login
+              </Button>
+              {/* </Link> */}
+              {/* {render ? success : fail} */}
+              {feedback}
+            </div>
+          </form>
         </div>
-        <div className="Password">
-          <label className = "labels" for="password">Password:</label>
-          <br />
-         <TextField className = "password"  id="PASSWORD" label="Password" name = "password" required />
-        </div>
-        <div className = "elly">
-        <Button type = "submit" className = "fullButton"  variant="contained" color="primary">
-       Login
-      </Button>
-      </div>
-      </form>
-      </div>
       </header>
     </div>
   );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import axios from "axios";
- import './Signin.css';
- import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import "./Signin.css";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+// import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [feedback, setFeedback] = useState();
+
+  const success = <div className="success1">Sign Up Successful!</div>;
+  // const fail = <div className="fail">Failed Sign Up.</div>;
+
   const submit = (e) => {
     e.preventDefault();
     let EMAIL = document.getElementById("EMAIL").value;
@@ -23,38 +29,65 @@ export default function SignUp() {
       })
       .then((res) => console.log(res))
       .catch((err) => console.log(err.response.data.message));
+    setFeedback(success);
   };
 
   return (
     <div className="signinn">
       <header className="App-header">
         <div className="signInPage">Sign Up</div>
-        <div className = "div">
-        <form onSubmit={submit} className="form">
-          <div className="input">
-            <label className= "labels" for="Email">Email:</label>
-            <br />
-             <TextField  className = "email" id="EMAIL" label="Email" name = "email" required/>
-          </div>
-          <div className="Password">
-            <label className= "labels" for="password">Password</label>
-            <br />
-           <TextField className = "password"  id="PASSWORD" label="Password" name = "password" required />
-          </div>
-          <div id="orgSelectContainer">
-            <select id="orgSelect">
-              <option value="Restaurant">Restaurant</option>
-              <option value="Charity" selected>
-                Charity
-              </option>
-            </select>
-          </div>
-          <div className = "elly">
-          <Button type = "submit" className = "fullButton"  variant="contained" color="primary">
-         Sign Up
-      </Button>
-      </div>
-        </form>
+        <div className="div">
+          <form onSubmit={submit} className="form">
+            <div className="input">
+              <label className="labels" for="Email">
+                Email:
+              </label>
+              <br />
+              <TextField
+                className="email"
+                id="EMAIL"
+                label="Email"
+                name="email"
+                required
+              />
+            </div>
+            <div className="Password">
+              <label className="labels" for="password">
+                Password
+              </label>
+              <br />
+              <TextField
+                className="password"
+                id="PASSWORD"
+                label="Password"
+                name="password"
+                required
+              />
+            </div>
+            <div id="orgSelectContainer">
+              <select id="orgSelect">
+                <option value="Restaurant">Restaurant</option>
+                <option value="Charity" selected>
+                  Charity
+                </option>
+              </select>
+            </div>
+            <div className="elly">
+              {/* <Link to="/signupcheck" style={{ textDecoration: "none" }}> */}
+              <Button
+                type="submit"
+                className="fullButton"
+                variant="contained"
+                color="primary"
+                required
+                // onClick={submit}
+              >
+                Sign Up
+              </Button>
+              {/* </Link> */}
+              {feedback}
+            </div>
+          </form>
         </div>
       </header>
     </div>
