0000ebc6d2785e206e54987b42da80d238bae9b0
Sarim Zia <username@email.com>
2021-01-24 03:32:00 -0500

    h

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -93,8 +93,8 @@ const rows = [
         <Button size="small">Your pickup/order status</Button>
       </CardActions>
     </Card>
-      
       <h2 className = "recentorders">Recent Order(s):</h2>
+      
     <TableContainer className = "hello" component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
@@ -121,6 +121,7 @@ const rows = [
         </TableBody>
       </Table>
     </TableContainer>
+  
 </div>
 </div>
 </div>
